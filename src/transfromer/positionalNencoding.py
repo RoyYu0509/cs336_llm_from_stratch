2b3f6c47a7838168aa8dd7b62eb3c6d21618d33c
Yifan <royyu0509@gmail.com>
2025-11-07 04:46:44 -0800

    fix bugs

--- src/transfromer/positionalNencoding.py
+++ src/transfromer/positionalNencoding.py
@@ -17,16 +17,21 @@ class RoPE:
         Create the rotation matrix
         """
         self.d_k = d_k
-        self.theta = torch.tensor(theta, device=device)
+        self.device = device
+        self.theta = torch.tensor(theta, device=device, dtype = torch.float32)
     
     def getRotMat(self, i:int, k:int):
-        theta_ik = i / (self.theta**((2*k)/self.d_k))
+        theta_ik = torch.tensor(i / (self.theta**((2*k)/self.d_k)), device=self.device)
+        _cos = torch.cos(theta_ik)
+        _sin = torch.sin(theta_ik)
+        
+        rotMat_ik: Float[Tensor, "out_vec in_vec"]
         rotMat_ik = torch.tensor(
             [
-             [torch.cos(theta_ik), -torch.sin(theta_ik)],
-             [torch.sin(theta_ik),  torch.cos(theta_ik)]
+             [_cos, -_sin],
+             [_sin,  _cos]
             ],
-            dtype=torch.float32
+            dtype=torch.float64
         )
         return rotMat_ik
 
@@ -38,46 +43,51 @@ class PosEncod:
         """
         self.theta = theta
         self.d_k = d_k
-        self.rotMatGen = RoPE(theta, d_k)
-        self.max_seq_len = max_seq_len
         self.device = device
-        
-    def _Ri(self, i, d):
-        """
-        Create the R^{dxd} Rotation Matrix for the R^{d} token vec
-        """
-        return torch.stack(
-            [
-             self.rotMatGen.getRotMat(i, k) for k in range(0, d//2)
-            ]
-        )
 
+        # Buffer the rotation matrix for reuse
+        self.Rs:Float[Tensor, "max_seq_l, half_d, 2out_vec, 2in_vec"]
+        if d_k % 2 != 0:
+            d = d_k+1   # Pad for odd d_k
+        else:
+            d = d_k
+        self.Rs = torch.stack(
+            [
+                torch.stack(
+                    [
+                        RoPE(theta, d_k).getRotMat(pos, k) for k in range(0, d//2)
+                    ],
+                    dim=0
+                ) for pos in range(max_seq_len)
+            ], 
+            dim = 0
+        )
     
     def forward(self, x: torch.Tensor, token_positions: torch.Tensor) -> torch.Tensor:
         """
-        Process an input tensor of shape (..., seq_len, d_k) and 
+        Process an in_vecput tensor of shape (..., seq_len, d_k) and 
         return a tensor of the same shape.
         """
+        
         x, token_positions = x.to(self.device), token_positions.to(self.device)
         # Pad a dummy dimension if the d_k is odd
         d = self.d_k
-        id_odd_d = d % 2 == 1
-        if id_odd_d:
+        is_odd_d = (d % 2 == 1)
+        if is_odd_d:
             x = torch.cat([x, torch.zeros(*x.shape[:-1], 1, device=self.device, dtype=x.dtype)], dim=-1)
             d += 1
-
-        Rs: Float[Tensor, f"seq, half_d, two_in, two_out"]
-        Rs = torch.stack(
-            [self._Ri(i, d) for i in range(self.max_seq_len)]
-        )
+        
+        # Get the Rotation Matrix
+        Rs: Float[Tensor, f"seq, half_d, in_vec, out_vec"]
+        Rs = self.Rs[token_positions].to(x.dtype) # Retrieve only the relavent position
         
         # Unpack x into several 2-dim vec
-        reshape_x = rearrange(x, "... seq (half_d sub_vec) -> ... seq half_d sub_vec", half_d = d//2, sub_vec = 2)
-        Rx = einsum(reshape_x, Rs, "... seq half_d sub_vec, seq half_d two_in two_out -> ... seq half_d two_out")
-        reshape_Rx = rearrange(Rx, "... seq half_d two_out -> ... seq (half_d two_out)")
+        reshape_x = rearrange(x, "... seq (half_d in_vec) -> ... seq half_d in_vec", half_d = d//2, in_vec = 2)
+        Rx = einsum(Rs, reshape_x, "seq half_d out_vec in_vec, ... seq half_d in_vec -> ... seq half_d out_vec")
+        reshape_Rx = rearrange(Rx, "... seq half_d out_vec -> ... seq (half_d out_vec)")
         # print(reshape_Rx.shape, x.shape)
-        if id_odd_d:
-            return reshape_Rx[..., :-1]
+        if is_odd_d:
+            return reshape_Rx[..., :self.d_k]
         else:
             return reshape_Rx
     
